68982c973f41f7ac41a7748a0225acabcc30de35
Absnautilus <absnautilus_online@protonmail.com>
2026-09-05 16:36:21 +0200

    feat: add src/app/App.tsx

---
+++ src/app/App.tsx
@@ -0,0 +1,21 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { ShellLayout } from '../components/ShellLayout'
+import { HomePage } from '../pages/HomePage'
+import { PlaceholderPage } from '../pages/PlaceholderPage'
+
+export function App() {
+  return (
+    <Routes>
+      <Route element={<ShellLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="housekeeping" element={<PlaceholderPage title="Housekeeping" />} />
+        <Route path="turni" element={<PlaceholderPage title="Turni" />} />
+        <Route path="transfer" element={<PlaceholderPage title="Transfer" />} />
+        <Route path="modules" element={<PlaceholderPage title="Moduli" />} />
+        <Route path="team" element={<PlaceholderPage title="Team" />} />
+        <Route path="settings" element={<PlaceholderPage title="Impostazioni" />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  )
+}
