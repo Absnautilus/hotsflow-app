f2e178df567ad483b91c42b0fef959f66b0dccbe
Absnautilus <absnautilus_online@protonmail.com>
2026-09-07 13:16:36 +0200

    feat: wire Team and Settings shell pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,8 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { ShellLayout } from '../components/ShellLayout'
 import { HomePage } from '../pages/HomePage'
 import { PlaceholderPage } from '../pages/PlaceholderPage'
+import { TeamPage } from '../pages/TeamPage'
+import { SettingsPage } from '../pages/SettingsPage'
 import { HousekeepingModuleGate } from '../modules/housekeeping/HousekeepingModuleGate'
 
 export function App() {
@@ -13,8 +15,8 @@ export function App() {
         <Route path="turni" element={<PlaceholderPage title="Turni" />} />
         <Route path="transfer" element={<PlaceholderPage title="Transfer" />} />
         <Route path="modules" element={<PlaceholderPage title="Moduli" />} />
-        <Route path="team" element={<PlaceholderPage title="Team" />} />
-        <Route path="settings" element={<PlaceholderPage title="Impostazioni" />} />
+        <Route path="team" element={<TeamPage />} />
+        <Route path="settings" element={<SettingsPage />} />
       </Route>
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
